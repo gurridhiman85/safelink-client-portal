f5a1af0b741e3a35da4d28ebd238153f7dc910c7
Gurri Dhiman <gurri.dhiman85@gmail.com>
2023-08-10 11:20:22 +0530

    code commit

--- config/mail.php
+++ config/mail.php
@@ -99,8 +99,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'noreply@safe-link.net'),
-        'name' => env('MAIL_FROM_NAME', 'Safelink'),
+        'address' => env('MAIL_FROM_ADDRESS', ''),
+        'name' => env('MAIL_FROM_NAME', ''),
     ],
 
     /*
